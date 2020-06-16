f7104edfe2e5da079c758499cc1a6007deeb11c0
Markus Felten <Markus.Felten@gmx.de>
2020-06-16 21:11:06 +0200

    chore: initial checkin

---
+++ src/gitlab-provider.mjs
@@ -0,0 +1,85 @@
+import fetch from "node-fetch";
+import { replaceWithOneTimeExecutionMethod } from "one-time-execution-method";
+
+import { MultiGroupProvider } from "repository-provider";
+
+/**
+ * Provider for gitlab repositories
+ *
+ */
+export class GitlabProvider extends MultiGroupProvider {
+  /**
+   * Default configuration as given for the cloud privider
+   * @return {Object}
+   */
+  static get attributes() {
+    return {
+      ...super.attributes,
+      api: {
+        description: "URL of the provider api",
+        env: "GITLAB_API",
+        default: "https://api.gitlab.com/"
+      }
+    };
+  }
+
+
+  /**
+   * All possible base urls
+   * @return {string[]} common base urls of all repositories
+   */
+  get repositoryBases() {
+    return [
+    ];
+  }
+
+  async initializeRepositories() {
+    let url = `repositories/?role=contributor`;
+
+    do {
+      const r = await this.fetch(url);
+      
+      if (!r.ok) {
+        break;
+      }
+
+      const res = await r.json();
+
+      url = res.next;
+      res.values.map(b => {
+        const groupName = b.owner.nickname || b.owner.username;
+        const group = this.addRepositoryGroup(groupName, b.owner);
+        group.addRepository(b.name, b);
+      });
+    } while (url);
+  }
+
+  fetch(url, options = {}) {
+    const headers = {
+      authorization:
+        "Basic " +
+        Buffer.from(
+          this.authentication.username + ":" + this.authentication.password
+        ).toString("base64"),
+      ...options.headers
+    };
+
+    if (options.data) {
+      options.body = JSON.stringify(options.data);
+      delete options.data;
+      headers["Content-Type"] = "application/json";
+    }
+
+    return fetch(new URL(url, this.api), {
+      ...options,
+      headers
+    });
+  }
+}
+
+replaceWithOneTimeExecutionMethod(
+  BitbucketProvider.prototype,
+  "initializeRepositories"
+);
+
+export default BitbucketProvider;
